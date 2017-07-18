6aabae68ccc690517e4194c3dae61b9bb481f533
parimatrix <parikansh.ahluwalia@gmail.com>
2017-07-18 09:55:53 +0530

    Initial Commit

---
+++ public_static/main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ParikanshAndAtikant on 16/07/2017.
+ */
+$(function () {
+
+    var socket = io();
+    var audio = document.getElementById('myaudio');
+
+    socket.on("first", function (data) {
+        audio.ontimeupdate = function () {
+            socket.emit("where", audio.currentTime);
+        };
+        $('#play').click(function () {
+            console.log("play");
+            $('#play').removeClass('show');
+            $('#play').addClass('hide');
+            $('#pause').removeClass('hide');
+            $('#pause').addClass('show');
+            socket.emit("play", audio.currentTime);
+        });
+        $('#pause').click(function () {
+            console.log("pause");
+            $('#pause').removeClass('show');
+            $('#pause').addClass('hide');
+            $('#play').removeClass('hide');
+            $('#play').addClass('show');
+            socket.emit("pause", audio.currentTime);
+        });
+    });
+    socket.on("current", function (data) {
+        var diff = audio.currentTime - data;
+        if (diff < 0 || diff > 2) {
+            audio.currentTime = data;
+        }
+    });
+    socket.on("playsong", function (data) {
+        audio.currentTime = data;
+        audio.play();
+        
+    });
+    socket.on("pausesong", function (data) {
+        audio.currentTime = data;
+        audio.pause();
+    });
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+/**
+ * Created by ParikanshAndAtikant on 16/07/2017.
+ */
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = process.env.PORT || 5000;
+var ctr=0;
+
+/*app.get('/',function (req,res) {
+   res.send("Hello world!");
+});*/
+app.use('/',express.static('public_static'));
+
+app.get('/changesong',function (req,res) {
+
+});
+io.on('connection',function (socket) {
+    console.log("connnected client");
+    if (ctr == 0) {
+        console.log("FirstPerson");
+        socket.emit("first", "first");
+    }
+    ctr++;
+    socket.on("disconnect", function () {
+        console.log("Client Disconnected");
+        ctr--;
+    });
+    socket.on("play",function (data) {
+       io.emit('playsong',data);
+    });
+    socket.on("pause",function (data) {
+        io.emit('pausesong',data);
+    });
+    socket.on("where",function (data) {
+        //console.log(data);
+        io.emit("current",data);
+    });
+});
+http.listen(PORT,function () {
+    console.log("server running on " + PORT);
+});
